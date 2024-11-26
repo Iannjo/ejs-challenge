354886a6025bf03e85da403833b803acd6bd8911
Ian Njoroge <iannjoro@gmail.com>
2024-11-26 18:33:53 +0100

    updating the delete route, changed the ejs files to read and output correct html tags

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ import _ from "lodash"
 let articles = [];
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const homeStartingContent = "<p>Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. </p>Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "<p>Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper.</p><p> Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
@@ -54,6 +54,7 @@ function isTitleUnique(title) {
       // Save new article
       const newArticle = { title, content };
       articles.push(newArticle);
+      console.dir(newArticle)
       
       console.log(`Article "${title}${content}" saved successfully.`);
       res.redirect("/")
@@ -101,7 +102,7 @@ app.post("/contact", function(req, res){
 
 //Dynamically making a url
 app.get("/posts/:postName",function(req,res){
-  console.dir(`the article name ${req.params.postName}`)
+  // console.dir(`the article name ${req.params.postName}`)
   //const requestedTitle = (req.params.postName).toLowerCase();
   //console.log(`requestedTitle ${requestedTitle}`)
 
@@ -115,53 +116,32 @@ app.get("/posts/:postName",function(req,res){
 });
 
 app.get("/edit/:postName",function(req,res){
-  console.dir(`the article name ${req.params.postName}`)
-  const requestedTitle = (req.params.postName).toLowerCase();
-
-  articles.forEach(function(article){
-    const storedTitle = _.lowerCase(article.title);
-    
-    if(storedTitle === requestedTitle){
-      console.dir("Match found");
-      console.dir(`Ẁe are in the edit ${article.content}`)
-      res.render('pages/edit',{title:article.title,body:article.content})
+  
+  articles.find((x)=>{
+    if(_.lowerCase(x.title) === req.params.postName.toLowerCase()){
       
-    } else {
-      console.dir("no cigar")
+      res.render('pages/edit',{title:x.title,body:x.content})
     }
-  });
+  })
+
+  
 
 });
 
 //The delete function
 app.get("/delete/:postName",function(req,res){
-  console.dir(`the article name ${req.params.postName}`)
-  const requestedTitle = (req.params.postName).toLowerCase();
-  const requestedTitle2 = (req.params.postName);
+  
+  const requestedTitle = req.params.postName;
   
   
-  
-  const index1 = articles.findIndex((obj => obj.title === requestedTitle2))
-  console.log(index1,requestedTitle,requestedTitle2);
+  //Find the index of the article
+  const artIndex = articles.findIndex((obj => obj.title === requestedTitle))
 
-  articles.forEach(function(article){
-    const storedTitle = _.lowerCase(article.title);
-    
-    if(storedTitle === requestedTitle){
-      console.dir("Match found");
-      console.dir(`Ẁe are in the delete ${article.content}`)
-      
-      const length = articles.length;
-      objIndex = articles.findIndex((obj => obj.title == requestedTitle));
-      console.log(objIndex,length);
-      delete articles[objIndex];
-      console.log(`Try again: ${length}`);
-      res.redirect("/")
-      
-    } else {
-      console.dir("no cigar")
-    }
-  });
+
+  //use the splice method to delete the article
+  articles.splice(artIndex,1)
+  res.redirect("/")
+  
 
 });
 
